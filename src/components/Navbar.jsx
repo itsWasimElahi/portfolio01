9594ef6f1c440ca4ae137fa4f69bfac0624de76f
WaseemElahi <elahi.mwaseem@gmail.com>
2024-05-13 00:49:19 +0500

    setting position 11

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -148,148 +148,6 @@
 
 
 
-// import React, { useState } from 'react';
-// import {
-//   FaBars,
-//   FaTimes,
-//   FaGithub,
-//   FaLinkedin,
-//   FaFacebook,
-//   FaLinkedinIn,
-// } from 'react-icons/fa';
-// import { HiOutlineMail } from 'react-icons/hi';
-// import { BsFillPersonLinesFill } from 'react-icons/bs';
-// import Plogo from '../assets/Plogo.jpg';
-// import { Link } from 'react-scroll';
-
-// const Navbar = () => {
-//   const [nav, setNav] = useState(false);
-//   const handleClick = () => setNav(!nav);
-
-//   return (
-//     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-//       <div>
-//         <img src={Plogo} alt='Logo Image' style={{ width: '50px' }} />
-//       </div>
-
-//       {/* menu */}
-//       <ul className='hidden md:flex'>
-//         <li>
-//           <Link to='home' smooth={true} duration={500}>
-//             Home
-//           </Link>
-//         </li>
-//         <li>
-//           <Link to='about' smooth={true} duration={500}>
-//             About
-//           </Link>
-//         </li>
-//         <li>
-//           <Link to='skills' smooth={true} duration={500}>
-//             Skills
-//           </Link>
-//         </li>
-//         <li>
-//           <Link to='work' smooth={true} duration={500}>
-//             Work
-//           </Link>
-//         </li>
-//         <li>
-//           <Link to='contact' smooth={true} duration={500}>
-//             Contact
-//           </Link>
-//         </li>
-//       </ul>
-
-//       {/* Hamburger */}
-//       <div onClick={handleClick} className='md:hidden z-10'>
-//         {!nav ? <FaBars /> : <FaTimes />}
-//       </div>
-
-//       {/* Mobile menu */}
-//       <ul
-//         className={
-//           !nav
-//             ? 'hidden'
-//             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
-//         }
-//       >
-//         <li className='py-6 text-4xl'>
-//           <Link onClick={handleClick} to='home' smooth={true} duration={500}>
-//             Home
-//           </Link>
-//         </li>
-//         <li className='py-6 text-4xl'>
-//           {' '}
-//           <Link onClick={handleClick} to='about' smooth={true} duration={500}>
-//             About
-//           </Link>
-//         </li>
-//         <li className='py-6 text-4xl'>
-//           {' '}
-//           <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
-//             Skills
-//           </Link>
-//         </li>
-//         <li className='py-6 text-4xl'>
-//           {' '}
-//           <Link onClick={handleClick} to='work' smooth={true} duration={500}>
-//             Work
-//           </Link>
-//         </li>
-//         <li className='py-6 text-4xl'>
-//           {' '}
-//           <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
-//             Contact
-//           </Link>
-//         </li>
-//       </ul>
-
-//       {/* Social icons */}
-//       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-//         <ul>
-//           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-//             <a
-//               className='flex justify-between items-center w-full text-gray-300'
-//               href='https://www.linkedin.com/in/m-waseem-elahi/'
-//             >
-//               Linkedin <FaLinkedin size={30} />
-//             </a>
-//           </li>
-//           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-//             <a
-//               className='flex justify-between items-center w-full text-gray-300'
-//               href='https://github.com/itsWasimElahi'
-//             >
-//               Github <FaGithub size={30} />
-//             </a>
-//           </li>
-//           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-//             <a
-//               className='flex justify-between items-center w-full text-gray-300'
-//               href='https://phenomenal-lily-c656cc.netlify.app/'
-//             >
-//               Email <HiOutlineMail size={30} />
-//             </a>
-//           </li>
-//           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-//             <a
-//               className='flex justify-between items-center w-full text-gray-300'
-//               href='https://drive.google.com/file/d/190odi0oMV2JJYfxzneRYDPWkH55UYN0n/view?usp=sharing'
-//             >
-//               Resume <BsFillPersonLinesFill size={30} />
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
-
-
 import React, { useState } from 'react';
 import {
   FaBars,
@@ -309,147 +167,289 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full bg-[#0a192f] text-gray-300'>
-      <div className='max-w-[1000px] mx-auto px-4 md:px-8 flex justify-between items-center h-[80px]'>
-        <div>
-          <img src={Plogo} alt='Logo Image' style={{ width: '50px' }} />
-        </div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={Plogo} alt='Logo Image' style={{ width: '50px' }} />
+      </div>
 
-        {/* Desktop menu */}
-        <ul className='hidden md:flex space-x-6'>
-          <li>
-            <Link to='home' smooth={true} duration={500}>
-              Home
-            </Link>
+      {/* menu */}
+      <ul className='hidden md:flex'>
+        <li>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Social icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://www.linkedin.com/in/m-waseem-elahi/'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
           </li>
-          <li>
-            <Link to='about' smooth={true} duration={500}>
-              About
-            </Link>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://github.com/itsWasimElahi'
+            >
+              Github <FaGithub size={30} />
+            </a>
           </li>
-          <li>
-            <Link to='skills' smooth={true} duration={500}>
-              Skills
-            </Link>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://phenomenal-lily-c656cc.netlify.app/'
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
           </li>
-          <li>
-            <Link to='work' smooth={true} duration={500}>
-              Work
-            </Link>
-          </li>
-          <li>
-            <Link to='contact' smooth={true} duration={500}>
-              Contact
-            </Link>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://drive.google.com/file/d/190odi0oMV2JJYfxzneRYDPWkH55UYN0n/view?usp=sharing'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
           </li>
         </ul>
-
-        {/* Hamburger */}
-        <div className='md:hidden z-10'>
-          <button onClick={handleClick}>
-            {!nav ? <FaBars /> : <FaTimes />}
-          </button>
-        </div>
-
-        {/* Mobile menu */}
-        {nav && (
-          <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
-            <li>
-              <Link
-                onClick={handleClick}
-                to='home'
-                smooth={true}
-                duration={500}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={handleClick}
-                to='about'
-                smooth={true}
-                duration={500}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={handleClick}
-                to='skills'
-                smooth={true}
-                duration={500}
-              >
-                Skills
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={handleClick}
-                to='work'
-                smooth={true}
-                duration={500}
-              >
-                Work
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={handleClick}
-                to='contact'
-                smooth={true}
-                duration={500}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        )}
-
-        {/* Social icons */}
-        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-          <ul>
-            <li>
-              <a
-                className='flex justify-between items-center w-[160px] h-[60px] text-gray-300 bg-blue-600 hover:ml-[-10px] duration-300'
-                href='https://www.linkedin.com/in/m-waseem-elahi/'
-              >
-                Linkedin <FaLinkedin size={30} />
-              </a>
-            </li>
-            <li>
-              <a
-                className='flex justify-between items-center w-[160px] h-[60px] text-gray-300 bg-[#333333] hover:ml-[-10px] duration-300'
-                href='https://github.com/itsWasimElahi'
-              >
-                Github <FaGithub size={30} />
-              </a>
-            </li>
-            <li>
-              <a
-                className='flex justify-between items-center w-[160px] h-[60px] text-gray-300 bg-[#6fc2b0] hover:ml-[-10px] duration-300'
-                href='https://phenomenal-lily-c656cc.netlify.app/'
-              >
-                Email <HiOutlineMail size={30} />
-              </a>
-            </li>
-            <li>
-              <a
-                className='flex justify-between items-center w-[160px] h-[60px] text-gray-300 bg-[#565f69] hover:ml-[-10px] duration-300'
-                href='https://drive.google.com/file/d/190odi0oMV2JJYfxzneRYDPWkH55UYN0n/view?usp=sharing'
-              >
-                Resume <BsFillPersonLinesFill size={30} />
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   );
 };
 
 export default Navbar;
+
+
+
+// import React, { useState } from 'react';
+// import {
+//   FaBars,
+//   FaTimes,
+//   FaGithub,
+//   FaLinkedin,
+//   FaFacebook,
+//   FaLinkedinIn,
+// } from 'react-icons/fa';
+// import { HiOutlineMail } from 'react-icons/hi';
+// import { BsFillPersonLinesFill } from 'react-icons/bs';
+// import Plogo from '../assets/Plogo.jpg';
+// import { Link } from 'react-scroll';
+
+// const Navbar = () => {
+//   const [nav, setNav] = useState(false);
+//   const handleClick = () => setNav(!nav);
+
+//   return (
+//     <div className='fixed w-full bg-[#0a192f] text-gray-300'>
+//       <div className='max-w-[1000px] mx-auto px-4 md:px-8 flex justify-between items-center h-[80px]'>
+//         <div>
+//           <img src={Plogo} alt='Logo Image' style={{ width: '50px' }} />
+//         </div>
+
+//         {/* Desktop menu */}
+//         <ul className='hidden md:flex space-x-6'>
+//           <li>
+//             <Link to='home' smooth={true} duration={500}>
+//               Home
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to='about' smooth={true} duration={500}>
+//               About
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to='skills' smooth={true} duration={500}>
+//               Skills
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to='work' smooth={true} duration={500}>
+//               Work
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to='contact' smooth={true} duration={500}>
+//               Contact
+//             </Link>
+//           </li>
+//         </ul>
+
+//         {/* Hamburger */}
+//         <div className='md:hidden z-10'>
+//           <button onClick={handleClick}>
+//             {!nav ? <FaBars /> : <FaTimes />}
+//           </button>
+//         </div>
+
+//         {/* Mobile menu */}
+//         {nav && (
+//           <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+//             <li>
+//               <Link
+//                 onClick={handleClick}
+//                 to='home'
+//                 smooth={true}
+//                 duration={500}
+//               >
+//                 Home
+//               </Link>
+//             </li>
+//             <li>
+//               <Link
+//                 onClick={handleClick}
+//                 to='about'
+//                 smooth={true}
+//                 duration={500}
+//               >
+//                 About
+//               </Link>
+//             </li>
+//             <li>
+//               <Link
+//                 onClick={handleClick}
+//                 to='skills'
+//                 smooth={true}
+//                 duration={500}
+//               >
+//                 Skills
+//               </Link>
+//             </li>
+//             <li>
+//               <Link
+//                 onClick={handleClick}
+//                 to='work'
+//                 smooth={true}
+//                 duration={500}
+//               >
+//                 Work
+//               </Link>
+//             </li>
+//             <li>
+//               <Link
+//                 onClick={handleClick}
+//                 to='contact'
+//                 smooth={true}
+//                 duration={500}
+//               >
+//                 Contact
+//               </Link>
+//             </li>
+//           </ul>
+//         )}
+
+//         {/* Social icons */}
+//         <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+//           <ul>
+//             <li>
+//               <a
+//                 className='flex justify-between items-center w-[160px] h-[60px] text-gray-300 bg-blue-600 hover:ml-[-10px] duration-300'
+//                 href='https://www.linkedin.com/in/m-waseem-elahi/'
+//               >
+//                 Linkedin <FaLinkedin size={30} />
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 className='flex justify-between items-center w-[160px] h-[60px] text-gray-300 bg-[#333333] hover:ml-[-10px] duration-300'
+//                 href='https://github.com/itsWasimElahi'
+//               >
+//                 Github <FaGithub size={30} />
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 className='flex justify-between items-center w-[160px] h-[60px] text-gray-300 bg-[#6fc2b0] hover:ml-[-10px] duration-300'
+//                 href='https://phenomenal-lily-c656cc.netlify.app/'
+//               >
+//                 Email <HiOutlineMail size={30} />
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 className='flex justify-between items-center w-[160px] h-[60px] text-gray-300 bg-[#565f69] hover:ml-[-10px] duration-300'
+//                 href='https://drive.google.com/file/d/190odi0oMV2JJYfxzneRYDPWkH55UYN0n/view?usp=sharing'
+//               >
+//                 Resume <BsFillPersonLinesFill size={30} />
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
 
 
 
