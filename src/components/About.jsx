d5524bf0663365b08847be81e7052349266f1e6d
WaseemElahi <elahi.mwaseem@gmail.com>
2024-05-13 17:30:51 +0500

    setting position 14

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
     <div name='about' className='w-full min-h-screen bg-[#0a192f] text-gray-300'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center h-full'>
         <div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
-          <div className='pb-8'>
+          <div className= 'sm:text-right pb-8 pl-4 '>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
               About
             </p>
